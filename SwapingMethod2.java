408c9519d0296c1caf1bb064804ce0c2c4ba0933
YashwanthReddy2004 <2311cs010669@mallareddyuniversity.ac.in>
2024-12-24 19:29:02 +0530

    Create SwapingMethod2.java

---
+++ SwapingMethod2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class SwapingMethod2 {
+    public static void main(String[] args){
+    Scanner Y = new Scanner(System.in);
+    int a,b;
+    System.out.println("Enter Values:");
+    a = Y.nextInt();
+    b = Y.nextInt();
+    System.out.println("Before Swapping:"+a+" "+b);
+    System.out.println("After Swapping:"+b+" "+a);
+    
+    }  
+}
